639b9251915221d7cc2d89729aa4f92e346ca386
marcusarmento <57120916+marcusarmento@users.noreply.github.com>
2024-06-24 11:40:00 -0300

    Atualização do código para suportar múltiplos anos e meses no CAGED

--- caged_downloader.py
+++ caged_downloader.py
@@ -1,9 +1,6 @@
-# caged_downloader.py
-
-from ftplib import FTP
 import os
-import shutil
 import pandas as pd
+from ftplib import FTP
 import py7zr
 import subprocess
 
@@ -28,8 +25,8 @@ class CagedDownloader:
         df = pd.read_csv(txt_path, delimiter=';')
         return df
 
-    def download_caged_data(self, years, months):
-        success_messages = []
+    def download_and_read_caged_data(self, years, months, uf, muni):
+        df_all = pd.DataFrame()
 
         for year in years:
             for month in months:
@@ -37,53 +34,28 @@ class CagedDownloader:
                 remote_archive = f'CAGEDMOV{year}{month}.7z'
                 remote_txt = f'CAGEDMOV{year}{month}.txt'
 
-                # Verificar se o arquivo está disponível para download
-                if self._file_exists(remote_directory, remote_archive):
-                    # Baixar arquivo compactado
+                # Verificar se o arquivo está disponível no servidor FTP
+                try:
                     with open(remote_archive, 'wb') as f:
                         self.ftp.retrbinary('RETR ' + os.path.join(remote_directory, remote_archive), f.write)
+                except:
+                    print(f"O arquivo {remote_archive} não está disponível para o ano {year} e mês {month}.")
+                    continue
 
-                    # Descompactar arquivo
-                    with py7zr.SevenZipFile(remote_archive, mode='r') as z:
-                        z.extractall()
+                # Descompactar arquivo
+                with py7zr.SevenZipFile(remote_archive, mode='r') as z:
+                    z.extractall()
 
-                    # Remover arquivos temporários
-                    os.remove(remote_archive)
+                # Ler arquivo de texto e filtrar por UF e município
+                df_month = self._read_txt_file(remote_txt)
+                if 'UF' in df_month.columns and 'Município' in df_month.columns:
+                    df_filtered = df_month[(df_month['uf'] == uf) & (df_month['município'] == muni)]
+                    df_all = pd.concat([df_all, df_filtered], ignore_index=True)
 
-                    # Mensagem de sucesso
-                    success_messages.append(f"Mês {month}/{year} baixado com sucesso.")
-                else:
-                    # Mensagem de erro se o arquivo não estiver disponível
-                    success_messages.append(f"Mês {month}/{year} ainda não está disponível.")
+                # Remover arquivos temporários
+                os.remove(remote_archive)
+                os.remove(remote_txt)
 
-        return success_messages
-
-    def _file_exists(self, remote_directory, filename):
-        file_list = self.ftp.nlst(remote_directory)
-        return filename in file_list
-
-    def download_and_read_caged_data(self, years, months, uf=None, muni=None):
-        df_all = pd.DataFrame()
-
-        # Baixar os dados
-        success_messages = self.download_caged_data(years, months)
-
-        for year in years:
-            for month in months:
-                remote_txt = f'CAGEDMOV{year}{month}.txt'
-
-                # Ler arquivo de texto se o download foi bem-sucedido
-                if f"Mês {month}/{year} baixado com sucesso." in success_messages:
-                    df_month = self._read_txt_file(remote_txt)
-
-                    # Filtrar por UF e município, se fornecidos
-                    if uf is not None and muni is not None:
-                        df_month = df_month[(df_month['uf'] == uf) & (df_month['município'] == muni)]
-
-                    # Empilhar os dados do mês em um DataFrame geral
-                    df_all = pd.concat([df_all, df_month], ignore_index=True)
-
-                    # Remover arquivo temporário
-                    os.remove(remote_txt)
+                print(f"Mês {month} do ano {year} baixado com sucesso.")
 
         return df_all
